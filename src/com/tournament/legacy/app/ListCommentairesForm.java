24e547e4c14246a5c2c3f579f16960b0202f1dff
AymenLaroussi <aymen.laroussi@live.fr>
2022-03-28 21:04:31 +0200

    Lister les commentaires

---
+++ src/com/tournament/legacy/app/ListCommentairesForm.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tournament.legacy.app;
+
+import com.codename1.components.SpanLabel;
+import com.codename1.ui.Button;
+import com.codename1.ui.Container;
+import com.codename1.ui.FontImage;
+import com.codename1.ui.Form;
+import com.codename1.ui.Label;
+import com.codename1.ui.table.TableLayout;
+import com.codename1.ui.util.Resources;
+import com.tournament.legacy.entites.Commentaires;
+import com.tournament.legacy.services.ServiceCommentaire;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Aymen Laroussi
+ */
+public class ListCommentairesForm extends Form {
+    Resources res;
+    Form current;
+    Button btnSuppriemr = new Button("X");
+    public ListCommentairesForm(Form previous){
+        setTitle("Liste des  commentaires");
+        add(new Label("Liste des  commentaires /n"));
+        
+        ArrayList<Commentaires> list = ServiceCommentaire.getInstance().ListeCommentaires();
+        for ( Commentaires c : list){
+            System.out.println(c.getMessage());
+            
+         add(new Label(c.getId()));   
+         add(new Label(c.getMessage()));
+        }
+          
+          
+        Form hi = new Form("Table Layout 2x2", new TableLayout(2, 2));
+        hi.add(new Label("First")).
+            add(new Label("Second")).
+            add(new Label("Third")).
+            add(new Label("Fourth")).
+            add(new Label("Fifth"));
+        hi.show();
+        
+
+        getToolbar().addMaterialCommandToLeftBar("",FontImage.MATERIAL_ARROW_BACK, e->previous.showBack());
+        
+        
+    }   
+}
--- src/services/ServiceCommentaire.java
+++ src/services/ServiceCommentaire.java
@@ -5,6 +5,7 @@
  */
 package com.tournament.legacy.services;
 import com.codename1.db.Database;
+import com.codename1.io.CharArrayReader;
 import com.tournament.legacy.utils.Statics;
 import com.tournament.legacy.entites.Commentaires;
 import com.codename1.io.ConnectionRequest;
@@ -12,7 +13,10 @@ import com.codename1.io.JSONParser;
 import com.codename1.io.NetworkEvent;
 import com.codename1.io.NetworkManager;
 import com.codename1.ui.events.ActionListener;
-
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -20,10 +24,13 @@ import com.codename1.ui.events.ActionListener;
  * @author Aymen Laroussi
  */
 public class ServiceCommentaire {
-     
+    
+    
+    
     public static ServiceCommentaire instance;
     public boolean resultOK;
     private ConnectionRequest req;
+    
     
     
     public ServiceCommentaire() {
@@ -50,6 +57,49 @@ public class ServiceCommentaire {
         NetworkManager.getInstance().addToQueueAndWait(req);
         return resultOK;        
     }
+    public ArrayList<Commentaires> ListeCommentaires() {
+              String url = Statics.BASE_URL + "/web/service/commentaires";
+              req.setUrl(url);
+              req.addResponseListener(new ActionListener<NetworkEvent>() {
+                  @Override
+                  public void actionPerformed(NetworkEvent evt) {
+                      try {
+                          coment = parseCommentaires(new String(req.getResponseData()));
+                          req.removeResponseListener(this);
+                          
+                      } catch (IOException ex) {
+
+                      }
+                  }
+              });
+              NetworkManager.getInstance().addToQueueAndWait(req);
+              return coment;
+          } 
+    public ArrayList<Commentaires> parseCommentaires(String jsonText) throws IOException {
+        try {
+            
+            coment = new ArrayList<>();
+            JSONParser j = new JSONParser();
+            Map<String, Object> tasksListJson = j.parseJSON(new CharArrayReader(jsonText.toCharArray()));
+
+            List<Map<String, Object>> list = (List<Map<String, Object>>) tasksListJson.get("root");
+            
+            for (Map<String, Object> obj : list) {
+                
+                Commentaires c = new Commentaires();
+                
+                
+                c.setId((obj.get("id").toString()));
+                c.setMessage((obj.get("message").toString()));
+                System.out.println(c);
+                
+                coment.add(c);
+            }
+        } catch (IOException ex) {
+        }
+        return coment;
+    }
+    
            
 }
 
